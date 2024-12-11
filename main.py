ca5c1452a05f4ffb4a7a8ab1bbb913fa7b5decb9
azu-cyber <avikalizhimomi@gmail.com>
2024-12-11 17:22:13 +0530

    A2

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ def printito10(n):
     if(n>10):
         return
     print(n)
-print1to10(1)
+    printito10(n+1)
+printito10(1)
+#project 2
+def fac(n):
+    if(n==1 or n==0):
+        return n*fac(n-1)
+n = int(input("Enter your number : "))
+print("Factorial of', n, "is : ",fac(n))
