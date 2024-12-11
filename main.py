a6a015d1d6ccede5eae335c22cbf3fe18ee02f05
azu-cyber <avikalizhimomi@gmail.com>
2024-12-11 17:07:35 +0530

    njndii2q

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from math import gcd
-def find_1cm(a, b):
-    return abs(a * b) // gcd(a, b)
-count = int(input("Enter the number of values: "))
-numbers = []
-for i in range(count):
-    number = int(input(f"Enter number fi + 1}: "))
-    numbers .append(number)
-print("LM of the numbers is:" find_1cm_multiple(numbers))
+def printito10(n):
+    if(n>10):
+        return
+    print(n)
+print1to10(1)
--- nsd/main.py
+++ nsd/main.py
@@ -14,3 +14,17 @@ for i in range(0,size):
     n = int(input("Enter element : "))
     set.append(n)
 printPowerSet(set, size)
+#no.2 project
+def totalFlips(number1, number2) :
+    flips = 0
+    while(number1 > 0 or number2 > 0):
+        t1 = (number1 & 1)
+        t2 = (number2 & 1)
+        if(t1 |= t2):
+            flips += 1
+        number1>>=1
+        number2>>=1
+    return False
+number1 = int(input("Enter First number : "))
+number2 = int(input("Enter Second number : "))
+print("\nNumber of flips needed : ",totalFlips(number1, number2))
